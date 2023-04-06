c91ede3d7f436d8ebe41a3907f4f79da4fe2c9f6
Justin <mbanefojustin@gmail.com>
2023-04-05 20:37:52 -0500

    js and test

---
+++ tests/Employee.test.js
@@ -0,0 +1,23 @@
+const { default: test } = require("node:test");
+const { describe } = require("yargs");
+// DID NOT ADD THE TWO LINES ABOVE 
+const Employee = require("../lib/Employee"); 
+
+//this is diff 
+const employee = new Employee ("Justin", 0306, "mbanefojustin@gmail.com")
+
+describe("Employee", () => { 
+    test("new employee", () => { 
+        expect(employee.getName().toEqual("Justin")); 
+    })
+    test("new employee id", () => { 
+        expect(employee.getId().toEqual("0306"))
+    })
+    test("new employee email", () => { 
+        expect(employee.getEmail().toEqual("mbanefojustin@gmail.com"))
+    })
+    test("new employee role", () => { 
+        expect(employee.getRole().toEqual("Employee"))
+    })
+})
+
---
+++ tests/Engineer.test.js
@@ -0,0 +1,12 @@
+const { default: test } = require("node:test");
+const { describe } = require("yargs");
+// DID NOT ADD THE TWO LINES ABOVE 
+const Engineer = require("../lib/Engineer"); 
+
+const newEgineer = ("Jay", 0903, "jay@gmail.com", "")
+
+describe("Engineer", () => { 
+    test("new engineer", () => { 
+        expect(engineer)
+    })
+})
